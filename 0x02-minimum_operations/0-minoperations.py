5ed898ae761ed9e2f899b835530347bac282a9f3
washucode <estherw8525@gmail.com>
2024-01-19 04:34:38 +0300

    fix 2

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -23,13 +23,16 @@ def minOperations(n):
             Will help apply Copy All Appropriately.
         """
         factors = []
-        for i in range(2, n + 1):
+        while n % 2 == 0:
+            factors.append(2)
+            n = n // 2
+        for i in range(3, int(n**0.5)+1, 2):
             while n % i == 0:
                 factors.append(i)
-                n /= i
-        if n > 1:
+                n = n // i
+
+        if n > 2:
             factors.append(n)
         return factors
-
     factors = prime_factors(n)
     return int(sum(factors))
