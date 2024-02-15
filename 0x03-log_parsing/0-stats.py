e84f71a562d89ddd6e84797ba972e24a28d707db
washucode <estherw8525@gmail.com>
2024-02-15 10:36:58 +0300

    correcrion log parsing

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -25,25 +25,33 @@ def metrics(lines):
     # initialize count of lines
     count = 0
 
-    for line in lines:
-        count += 1
-        data = line.split()
-        try:
-            file_size += int(data[-1])
-        except(ValueError):
-            pass
-        try:
-            status_codes[data[-2]] += 1
-        except(KeyError):
-            pass
-
-    print("File size: {}".format(file_size))
-
-    for key, value in sorted(status_codes.items()):
-        if value != 0:
-            print("{}: {}".format(key, value))
+    def print_stats(status_codes, file_size):
+        """Prints the stats"""
+        print("File size: {}".format(file_size))
+        for key, value in sorted(status_codes.items()):
+            if value != 0:
+                print("{}: {}".format(key, value))
+    try:
+        for line in lines:
+            count += 1
+            data = line.split()
+            try:
+                file_size += int(data[-1])
+            except BaseException:
+                pass
+            try:
+                if data[-2] in status_codes:
+                    status_codes[data[-2]] += 1
+            except BaseException:
+                pass
+            if count % 10 == 0:
+                print_stats(status_codes, file_size)
+        print_stats(status_codes, file_size)
+    except KeyboardInterrupt:
+        print_stats(status_codes, file_size)
+        pass
 
 
 if __name__ == "__main__":
-    lines = sys.stdin.readlines()
+    lines = sys.stdin
     metrics(lines)
